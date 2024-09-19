a01935ceec6e3f45248e81f5e4ea2400083847c4
faddajinx <jomo.darlington@gmail.com>
2024-09-19 18:47:10 +0000

    terraform and tfvars with Azure

---
+++ azure-tf-tour/outputs.tf
@@ -0,0 +1,5 @@
+# return a value returned by our module
+output "rg_id" {
+  value       = azurerm_resource_group.rg.id
+  description = "ID of the resource group"
+}
---
+++ azure-vnettfvars/campus_beta.tfvars
@@ -0,0 +1,3 @@
+name="skynet2"
+location="westus3"
+address_space="10.40.0.0/16"
